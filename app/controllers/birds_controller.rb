070209eef4fdd51d5e7d591c0427f3edf977a705
James <jamesnjorio@gmail.com>
2022-11-23 10:11:47 +0300

    Corrected an error

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -19,7 +19,7 @@ class BirdsController < ApplicationController
   def create 
     # byebug
     bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird. status: :created
+    render json: bird, status: :created
   end
 
 end
